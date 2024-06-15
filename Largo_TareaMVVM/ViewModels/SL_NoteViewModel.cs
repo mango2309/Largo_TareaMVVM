b80e141e813852ad90df996e7cee0f4d98dac802
mango2309 <sebastian.largo@udla.edu.ec>
2024-06-15 10:53:44 -0500

    Modificacion de la clase NoteViewModel y su vista

--- Largo_TareaMVVM/ViewModels/SL_NoteViewModel.cs
+++ Largo_TareaMVVM/ViewModels/SL_NoteViewModel.cs
@@ -1,12 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+using System.Reflection;
 
 namespace Largo_TareaMVVM.ViewModels
 {
-    class SL_NoteViewModel
+    internal class SL_NoteViewModel : ObservableObject, IQueryAttributable
     {
+        private SL_Models.SL_Note _note;
+
+        public string Text
+        {
+            get => _note.Text;
+            set
+            {
+                if (_note.Text != value)
+                {
+                    _note.Text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime Date => _note.Date;
+
+        public string Identifier => _note.Filename;
+
+        public ICommand SaveCommand { get; private set; }
+        public ICommand DeleteCommand { get; private set; }
+
+        public SL_NoteViewModel()
+        {
+            _note = new SL_Models.SL_Note();
+            SaveCommand = new AsyncRelayCommand(Save);
+            DeleteCommand = new AsyncRelayCommand(Delete);
+        }
+
+        public SL_NoteViewModel(SL_Models.SL_Note note)
+        {
+            _note = note;
+            SaveCommand = new AsyncRelayCommand(Save);
+            DeleteCommand = new AsyncRelayCommand(Delete);
+        }
+
+        private async Task Save()
+        {
+            _note.Date = DateTime.Now;
+            _note.Save();
+            await Shell.Current.GoToAsync($"..?saved={_note.Filename}");
+        }
+
+        private async Task Delete()
+        {
+            _note.Delete();
+            await Shell.Current.GoToAsync($"..?deleted={_note.Filename}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("load"))
+            {
+                _note = SL_Models.SL_Note.Load(query["load"].ToString());
+                RefreshProperties();
+            }
+        }
+
+        public void Reload()
+        {
+            _note = SL_Models.SL_Note.Load(_note.Filename);
+            RefreshProperties();
+        }
+
+        private void RefreshProperties()
+        {
+            OnPropertyChanged(nameof(Text));
+            OnPropertyChanged(nameof(Date));
+        }
+
     }
 }
