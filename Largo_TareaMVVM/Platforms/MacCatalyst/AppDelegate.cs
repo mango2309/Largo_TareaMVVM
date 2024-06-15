c1085dc4757cb54d3d9fd976c3de52beacdfb68c
mango2309 <sebastian.largo@udla.edu.ec>
2024-06-14 23:35:05 -0500

    Agregar archivos de proyecto.

---
+++ Largo_TareaMVVM/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Largo_TareaMVVM
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ Largo_TareaMVVM/SL_Models/SL_AllNotes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Android.Provider.ContactsContract.CommonDataKinds;
+
+
+namespace Largo_TareaMVVM.SL_Models
+{
+    internal class SL_AllNotes
+    {
+        public ObservableCollection<SL_Note> Notes { get; set; } = new ObservableCollection<SL_Note>();
+
+        public SL_AllNotes() =>
+            LoadNotes();
+
+        public void LoadNotes()
+        {
+            Notes.Clear();
+            // Get the folder where the notes are stored.
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            // Use Linq extensions to load the *.notes.txt files.
+            IEnumerable<SL_Note> notes = Directory
+
+                                        // Select the file names from the directory
+                                        .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                                        // Each file name is used to create a new Note
+                                        .Select(filename => new SL_Note()
+                                        {
+                                            Filename = filename,
+                                            Text = File.ReadAllText(filename),
+                                            Date = File.GetLastWriteTime(filename)
+                                        })
+
+                                        // With the final collection of notes, order them by date
+                                        .OrderBy(note => note.Date);
+
+            // Add each note into the ObservableCollection
+            foreach (SL_Note note in notes)
+                Notes.Add(note);
+        }
+    }
+}
