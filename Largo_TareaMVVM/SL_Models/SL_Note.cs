c7fb6392e831360f0af5d7d23e89fd710d6946ad
mango2309 <sebastian.largo@udla.edu.ec>
2024-06-14 23:56:21 -0500

    Modificacion y adicion de elementos a la clase Note

--- Largo_TareaMVVM/SL_Models/SL_Note.cs
+++ Largo_TareaMVVM/SL_Models/SL_Note.cs
@@ -1,15 +1,68 @@
-﻿using System;
+﻿using Microsoft.Maui.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Largo_TareaMVVM.SL_Models
 {
     internal class SL_Note
     {
-        public string? Filename { get; set; }
-        public string? Text { get; set; }
+        public string Filename { get; set; }
+        public string Text { get; set; }
         public DateTime Date { get; set; }
+
+        public SL_Note()
+        {
+            Filename = $"{Path.GetRandomFileName()}.notes.txt";
+            Date = DateTime.Now;
+            Text = "";
+        }
+
+
+        public void Save() =>
+        File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+        public void Delete() =>
+        File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+        public static SL_Note Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+            return
+                new()
+                {
+                    Filename = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                };
+        }
+
+        public static IEnumerable<SL_Note> LoadAll()
+        {
+            // Get the folder where the notes are stored.
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            // Use Linq extensions to load the *.notes.txt files.
+            return Directory
+
+                    // Select the file names from the directory
+                    .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                    // Each file name is used to load a note
+                    .Select(filename => SL_Note.Load(Path.GetFileName(filename)))
+
+                    // With the final collection of notes, order them by date
+                    .OrderByDescending(note => note.Date);
+        }
+
     }
+
 }
